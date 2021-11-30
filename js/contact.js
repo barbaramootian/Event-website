ef03544c4bc69ffd0a3814bf34179e3969c1a7dd
barbaramootian <barbaramootian@student.moringaschool.com>
2021-11-30 19:43:42 +0300

    added alert function

---
+++ js/contact.js
@@ -0,0 +1,13 @@
+function Message() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var message = document.getElementById("comment").value;
+    if (email !== "" && name !== "" && message !== "") {
+        alert(
+            "Hello" + "" + ""  + name +  " Thanks for reaching out to Endless Events. We have received your Message!."
+        );
+    } else if (email == "" || name !== ""  || phone !== "" || message !== "") {
+        alert("Please fill in  all the field");
+    }
+}
