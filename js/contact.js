77261e9e9ac924b1a3e2e74dff70bc65c1c1b162
barbaramootian <barbaramootian@student.moringaschool.com>
2021-12-01 14:03:56 +0300

    added function to the form

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,4 @@
-function Message() {
+function validateform() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var phone = document.getElementById("phone").value;
